770d4ce0a436dac2ce80c5a982b450d5f9aed392
Yiling <yilingchen1@gmail.com>
2015-05-14 16:54:19 -0700

    winning conditions written

--- javascript/board.js
+++ javascript/board.js
@@ -11,7 +11,7 @@
   Board.prototype.markHumanPosition = function (row, col) {
     this.grid[row][col] = "X";
     this.game.changePlayer();
-    
+
     if (!this.over()) {
       this.game.playComputerMove();
     }
@@ -20,7 +20,7 @@
 
   Board.prototype.markComputerPosition = function() {
     // get winning/non-losing move
-    // add html/css to the correct tile
+
     var computer = new TTT.Computer({board: this, game: this.game});
     var row = computer.getMove()[0];
     var col = computer.getMove()[1];
@@ -35,10 +35,40 @@
   };
 
   Board.prototype.makeGrid = function() {
-    return [[0, 0 , 0], [0, 0 ,0], [0, 0, 0]];
+    return [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  };
+
+  Board.prototype.won = function() {
+    for (var row = 0; row < this.grid.length; row++) {
+      if (this.allEqual(this.grid[row])) {
+        return true;
+      }
+      if (this.allEqual([this.grid[0][row], this.grid[1][row], this.grid[2][row]])) {
+        return true;
+      }
+    }
+    if (this.allEqual([this.grid[0][0], this.grid[1][1], this.grid[2][2]])) {
+      return true;
+    }
+    else if (this.allEqual([this.grid[2][0], this.grid[1][1], this.grid[0][2]])) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  Board.prototype.allEqual = function(arr) {
+    if ((arr[0] === arr[1] && arr[1] === arr[2]) && arr[0] !== 0) {
+      return true;
+    }
+    return false;
   };
 
   Board.prototype.over = function() {
+    if (this.won()) {
+      return true;
+    }
     var flattened = [];
     flattened = flattened.concat.apply(flattened, this.grid);
     for (var i = 0; i < flattened.length; i++) {
